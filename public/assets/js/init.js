3668bcf2ce117617dbedc032604e8737212a3040
aksm <albert.min@gmail.com>
2016-11-10 18:10:35 -0500

    Functioning but disconnected front-end and back-end.

---
+++ public/assets/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+	  $('.chips-placeholder').material_chip({
+	    placeholder: '+Burger',
+	    secondaryPlaceholder: 'Prepare a Burger'
+	  });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+var app = express();
+// var dotenv = require("dotenv");
+// dotenv.load();
+
+// Set port
+app.set("port", process.env.PORT || 8080);
+
+// Set static
+app.use("/css",express.static(__dirname + "/public/assets/css"));
+app.use("/fonts",express.static(__dirname + "/public/assets/fonts"));
+app.use("/js",express.static(__dirname + "/public/assets/js"));
+app.use("/images",express.static(__dirname + "/public/assets/images"));
+
+// Set handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.get("/", function(req, res) {
+	res.render("index");
+});
+
+// Listener
+app.listen(app.get("port"), function() {console.log("Hollaback on port: "+app.get("port"));});

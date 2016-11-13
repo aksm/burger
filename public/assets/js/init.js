4c64662850ba64478afc4a657671ea42b5d34ec7
aksm <albert.min@gmail.com>
2016-11-13 16:40:36 -0500

    Add commenting

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -1,8 +1,8 @@
 (function($){
   $(function(){
+  	  // Get all burgers
   	  $.get("/getBurgers").
   	  done(function(data) {
-  	  	console.log(data);
   	  	$.each(data, function(index, value) {
   	  		if(value.devoured === 1) {
 			  	$("#burgers-devoured").append("<div class='row burger'><div class='chip col offset-s6 offset-m6'><img src='/images/burger.png' alt='burger'/>"+value.burger_name);
@@ -13,17 +13,17 @@
   	  // Materialize chip listeners
 	  $('.chips-placeholder').material_chip({
 	    placeholder: '+Burger',
-	    secondaryPlaceholder: 'Prepare a Burger'
+	    secondaryPlaceholder: 'Prepare a Burger (Enter a burger you want)'
 	  });
+	  // Add user-input burgers
 	  $('.chips-placeholder').on('chip.add', function(e, chip){
-	  	// console.log(chip);
 	  	$.post("/addBurger", chip).
 	  	done(function(data) {
 	  		chip.id = data;
 	  	});
 	  	Materialize.toast('Click mouth to eat!', 4000);
 	  });
-
+	  // Move burger to stomach
 	  $('.chips-placeholder').on('chip.delete', function(e, chip){
 	  	$.post("/eatBurger", chip).
 	  	done(function(data) {
